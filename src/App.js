4f14852c78cb40d13d4846e30254c2ac9e818897
nshutijonathan <nshutijonathan130@gmail.com>
2019-11-10 09:23:52 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+class App extends Component {
+  render() {
+    return (
+      
+        <div>
+          <p>Hello</p>
+        </div>
+      
+    );
+  }
+}
+export default App;
